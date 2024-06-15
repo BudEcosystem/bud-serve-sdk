c72866e874e811a2d9fdf7b3c7346feea686a960
Ditto P S <resources@accubits.com>
2024-06-15 21:12:24 +0530

    update guidance example

--- examples/guidance_example.py
+++ examples/guidance_example.py
@@ -1,7 +1,7 @@
 from guidance import gen, user, system, assistant
 from budserve.models.guidance import BudServeClient
 
-llama2 = BudServeClient("meta-llama/Llama-2-7b-hf", echo=False, base_url="http://20.51.203.32:9000/v1")
+llama2 = BudServeClient("microsoft/Phi-3-medium-4k-instruct", echo=False, base_url="http://x.x.x.x:9000/v1")
 
 with user():
     llama2 += f'what is your name? '
