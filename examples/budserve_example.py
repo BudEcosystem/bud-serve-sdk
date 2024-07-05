aecb86eeb1fb449766edf66989c99de7edf5141a
dittops <dittops@gmail.com>
2024-07-05 23:07:32 +0530

    API integration with the engine

--- examples/budserve_example.py
+++ examples/budserve_example.py
@@ -1,6 +1,6 @@
 from budserve import BudServe
 
-client = BudServe(base_url="http://serve-lab.bud.studio/v1")
+client = BudServe(base_url="http://x.x.x.x:xxxx/v1")
 
 stream = client.chat.completions.create(
     model="meta-llama/Llama-2-7b-chat-hf",
@@ -8,6 +8,6 @@ stream = client.chat.completions.create(
     stream=True,
     max_tokens=200
 )
-print(stream.choices[0].message.content)
+
 for chunk in stream:
     print(chunk.choices[0].delta.content or "", end="", flush=True)
